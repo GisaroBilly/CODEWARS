5022e9b897e7d28744fbf95b233c3fbd3a9621e8
GisaroBilly <kingcccxlvii@gmail.com>
2026-06-01 08:38:36 +0200

    Add solution - CodeHub

---
+++ 6-kyu/replace-with-alphabet-position/replace-with-alphabet-position.js
@@ -0,0 +1,8 @@
+const alphabetPosition = text => {
+return text
+        .toLowerCase()
+        .replace(/[^a-z]/g,'')
+        .split('')
+        .map(char => char.charCodeAt(0)-96)
+        .join(' ')
+}
